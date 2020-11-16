d2d749f977d0ecd002b4466c205db9e88310d524
Anatoly <antauren@yandex.ru>
2020-11-16 17:00:37 +0300

    add add_product_to_cart func

--- elasticpath.py
+++ elasticpath.py
@@ -37,3 +37,24 @@ def get_cart(access_token: str, user: str) -> dict:
 
     response_dict = response.json()
     return response_dict
+
+
+def add_product_to_cart(access_token: str, customer: str, product_id: str, quantity: int):
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        'data': {
+            'id': product_id,
+            'type': 'cart_item',
+            'quantity': quantity,
+        }
+    }
+
+    response = requests.post('https://api.moltin.com/v2/carts/{}/items'.format(customer), headers=headers, json=data)
+    response.raise_for_status()
+
+    response_dict = response.json()
+    return response_dict
